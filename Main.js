7e873750efbf7976d58fc3e2e97956f6224541d5
Jayden Tucker <jaydenrtucker@gmail.com>
2020-08-17 20:47:44 -0700

    Main javascript file for gameplay. Currently prints a message to the screen when a button is clicked for testing purposes.

---
+++ Main.js
@@ -0,0 +1,10 @@
+let attackButton = document.getElementById("attack");
+
+let turnHistory = document.getElementById("turnHistory");
+
+function attack(){
+
+  turnHistory.innerHTML += "<div class='turn'>Attack<div>";
+
+}
+
