c2774956a9b4982e0e934d4a5348f4dd65764195
Jayden Tucker <jaydenrtucker@gmail.com>
2020-08-18 20:42:24 -0700

    Changed the attack function to take in an x and y input, and to print the coordinates to the turn history. Also added a function to spawn the grid on screen

--- Main.js
+++ Main.js
@@ -2,9 +2,30 @@ let attackButton = document.getElementById("attack");
 
 let turnHistory = document.getElementById("turnHistory");
 
-function attack(){
+//Finds the grid div and places one button for each tile on the grid
+const placeGrid = function(){
 
-  turnHistory.innerHTML += "<div class='turn'>Attack<div>";
+  let grid = document.getElementById("grid");
+
+  for(let y= 1; y <= 10; y++){
+
+    for(let x = 1; x <= 10; x++){
+
+      grid.innerHTML += `<button onclick="attack(${x},${y})"></button>`;
+
+    }
+
+    grid.innerHTML += "<br>";
+
+  }
 
 }
 
+const attack = function(x, y){
+
+  turnHistory.innerHTML += `<div class='turn'>P1 Attacks ${x}, ${y}: <div>`;
+
+}
+
+//+++Start Game+++
+placeGrid();
