2e3fe4f416e7921769b6f47c19ad3595d025eb93
ninjabattler <jaydenrtucker@gmail.com>
2020-08-25 03:43:34 +0000

    Added the ablility to move a ship around the grid and switch between a horizontal or verticle rotation. Cannot place ships yet

--- Main.js
+++ Main.js
@@ -30,6 +30,73 @@ const playerGrid = [
   [false, false, false, false, false, false, false, false, false, false]
 ]
 
+let playerShips = {
+
+  carrier: {
+
+    position: [1,1],
+
+    hits: 5
+
+  },
+
+  battleship: {
+
+    position: [1,1],
+
+    hits: 4
+
+  },
+
+  cruiser: {
+
+    position: [1,1],
+
+    hits: 3
+
+  }, 
+
+  submarine: {
+
+    position: [1,1],
+
+    hits: 3
+
+  }, 
+
+  destroyer: {
+
+    position: [1,1],
+
+    hits: 2
+
+  }
+
+}
+
+let verticle = true;
+
+const rotate = function(){
+
+  verticle = !verticle;
+
+  moveShip("battleship",0,0);
+
+}
+
+const resetGrid = function(){
+
+  for(let y= 1; y <= 10; y++){
+
+    for(let x = 1; x <= 10; x++){
+
+        document.getElementById(`gB${x + (y*10)}`).outerHTML = `<button class="gridButton" id="gB${x + (y*10)}" onclick="attack(${x},${y},'P1')">${columnNames[x-1]}${y}</button>`;
+
+    }
+
+  }
+}
+
 //Finds the grid div and places one button for each tile on the grid
 const placeGrid = function(grid, enemy){
 
@@ -53,6 +120,46 @@ const placeGrid = function(grid, enemy){
 
   }
 
+};
+//Script for placing a ship on the grid
+const moveShip = function(ship, x, y){
+
+  resetGrid();
+
+  playerShips[ship].position[0] += x;
+  playerShips[ship].position[1] += y;
+
+  if(!verticle){
+
+    if(playerShips[ship].position[0] + playerShips[ship].hits > 11 || playerShips[ship].position[0] + playerShips[ship].hits < 5){
+
+      playerShips[ship].position[0] -= x;
+
+    }
+
+  } else if(playerShips[ship].position[1] + playerShips[ship].hits > 11 || playerShips[ship].position[1] + playerShips[ship].hits < 5){
+
+    playerShips[ship].position[1] -= y;
+
+  }
+
+  let posX = playerShips[ship].position[0];
+  let posY = playerShips[ship].position[1];
+  
+  if(!verticle){
+    for(let i = 0; i < playerShips[ship].hits; i++){
+
+      document.getElementById(`gB${(posX) + ((posY*10) + i)}`).className = "placing";
+
+    }
+  } else {
+    for(let i = 0; i < playerShips[ship].hits; i++){
+
+      document.getElementById(`gB${(posX) + ((posY*(10)) + 10*i)}`).className = "placing";
+  
+    }
+  }
+
 }
 
 const attack = function(x, y, attacker, enemy){
@@ -61,7 +168,7 @@ const attack = function(x, y, attacker, enemy){
   if(enemy) button = document.getElementById(`eB${x + (y*10)}`);
 
   let gridToAttack = enemyGrid;
-  if(enemy) gridToAttack = playerGrid
+  if(enemy) gridToAttack = playerGrid;
 
   if(gridToAttack[y-1][x-1]){
 
@@ -79,10 +186,12 @@ const attack = function(x, y, attacker, enemy){
 
   button.setAttribute("onClick", "");
 
-  if(!enemy) {setTimeout(() => {attack(Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10),"P2", true)}, 1000);}
+  if(!enemy) {setTimeout(() => {attack(Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10),"P2", true)}, 1000);};
 
 }
 
 //+++Start Game+++
 placeGrid(document.getElementById("grid"), false);
 placeGrid(document.getElementById("grid2"), true);
+
+moveShip("battleship", 1, 5);
